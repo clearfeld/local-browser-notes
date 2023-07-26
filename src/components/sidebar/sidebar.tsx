a9e6d47e5310c3055612a9f6f18177f9b3412c1c
Marko <49600278+clearfeld@users.noreply.github.com>
2023-07-26 17:29:42 -0400

    Add: new theme toggle button and clean up sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./sidebar.scss";
 
 import Cookies from "js-cookie";
+
+import { ReactComponent as ThemeLightSVG } from "../editor/assets/theme-light.svg";
+import { ReactComponent as ThemeDarkSVG } from "../editor/assets/theme-dark.svg";
 
 interface I_Cookie_UserPreferences {
 	theme: "Dark" | "Light" | string;
@@ -11,6 +14,8 @@ interface I_Cookie_UserPreferences {
 function Sidebar() {
 	const user_preferences_cookie = "lbn__user_preferences";
 	const domain_str = ".temp-domain.io";
+
+	const [theme, setTheme] = useState<"DARK" | "LIGHT">("LIGHT");
 
 	useEffect(() => {
 		EnablePreferredTheme();
@@ -25,8 +30,10 @@ function Sidebar() {
 			const htmlroot = document.getElementsByTagName("html")[0];
 
 			if (upc.theme === "Dark") {
+				setTheme("DARK");
 				htmlroot.setAttribute("data-theme", "Dark");
 			} else {
+				setTheme("LIGHT");
 				htmlroot.setAttribute("data-theme", "Light");
 			}
 		}
@@ -38,9 +45,11 @@ function Sidebar() {
 		const dt = htmlroot.getAttribute("data-theme");
 		let theme = "";
 		if (dt === "Dark") {
+			setTheme("LIGHT");
 			htmlroot.setAttribute("data-theme", "Light");
 			theme = "Light";
 		} else {
+			setTheme("DARK");
 			htmlroot.setAttribute("data-theme", "Dark");
 			theme = "Dark";
 		}
@@ -70,16 +79,42 @@ function Sidebar() {
 
 	return (
 		<div className="sidebar__wrapper">
-			<div
-                onClick={ToggleThemes}
-                onKeyDown={ToggleThemes}
-                role="button"
-                tabIndex={0}
-            >
-				Theme Toggle
+			<div className="sidebar__top-wrapper">
+				<p className="sidebar__title-main">LB Notes</p>
+
+				<div
+					className="sidebar__icon-btn"
+					onClick={ToggleThemes}
+					onKeyDown={ToggleThemes}
+					role="button"
+					tabIndex={0}
+					title="Toggle Theme"
+				>
+					{theme === "DARK" ? (
+						<ThemeDarkSVG
+							className="svg-filter"
+							viewBox="0 0 24 24"
+							height="1.25rem"
+							width="1.25rem"
+						/>
+					) : (
+						<ThemeLightSVG
+							className="svg-filter"
+							viewBox="0 0 24 24"
+							height="1.25rem"
+							width="1.25rem"
+						/>
+					)}
+				</div>
 			</div>
 
-			<div>Send feedback</div>
+			<div className="sidebar__spacer" />
+
+			<hr className="sidebar__divider" />
+
+			<a href="https://github.com/clearfeld/local-browser-notes">
+				<div className="sidebar__github-link">Github</div>
+			</a>
 		</div>
 	);
 }
